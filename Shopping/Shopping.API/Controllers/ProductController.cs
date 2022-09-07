c8d1d971edf569a72ec6e3c0acd656d5744d3688
Evan Klein <Evan.Klein@Milliman.com>
2022-09-06 21:36:10 -0400

    Initial commit

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using Shopping.API.Data;
+using Shopping.API.Models;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly ProductContext _productContext;
+
+        public ProductController(ILogger<ProductController> logger, ProductContext context)
+        {
+            _logger = logger;
+            _productContext = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _productContext.Products
+                                        .Find(p => true)
+                                        .ToListAsync();
+        }
+    }
+}
